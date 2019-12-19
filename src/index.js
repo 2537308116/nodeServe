05bf1adde8427437bff1499154c2db0a97cf796a
xiaopeng <2537308116@qq.com>
2019-12-19 18:45:34 +0800

    rudex初步配置

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router/router';
 import * as serviceWorker from './serviceWorker';
+import store from '../src/rudex/store.js'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+    <Router />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
